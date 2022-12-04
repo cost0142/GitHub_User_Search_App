1a1d2bef0653630b96ba61d1d5f4892dcc1029f0
Hygor Costa <cost0142@algonquinlive.com>
2022-12-04 00:08:30 -0500

    Build About View Screen

--- hygor_Costa_Final/Views/AboutView.swift
+++ hygor_Costa_Final/Views/AboutView.swift
@@ -13,11 +13,68 @@ import SwiftUI
 // Name
 // etc.......
 
+
+
+
+
+//struct AboutView: View {
+//    var body: some View {
+//        Text("BENNNN, World!")
+//    }
+//}
+//
+//struct AboutView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AboutView()
+//    }
+//}
+
 struct AboutView: View {
     var body: some View {
-        Text("Hello, World!")
+        ScrollView{
+            VStack{
+                HStack{
+                    Image("HygorFoto").resizable()
+                        .frame(width: 150, height: 150)
+                        .mask(Circle())
+                        .padding(.top, 60)
+                }
+                .padding(.horizontal)
+                .padding(.bottom,50)
+                VStack{
+                    Text("GitHub SearchApp")
+                        .padding(.bottom, 5)
+                    Text("Version : 1.0")
+                        .padding(.bottom, 5)
+                    Text("Created by Hygor Costa")
+                        .padding(.bottom, 5)
+                        .multilineTextAlignment(.center)
+                    Text("MAD9137")
+                        .padding(.bottom, 10)
+                    VStack(alignment: .leading){
+                        HStack{
+                            Image(systemName: "envelope")
+                            Link("cost0142@algonquinlive.com", destination: URL(string: "mailto:cost0142@algonquinlive.com")! )
+                                .accentColor(.lightGreen)
+                                .frame(width: 250)
+                                .padding(.bottom, 2)
+                        }
+                        HStack{
+                            Image(systemName: "link")
+                            Link("https://github.com/cost0142", destination: URL(string: "https://github.com/cost0142")! )
+                                .accentColor(.lightGreen)
+                                .frame(width: 250)
+                        }
+                    }.padding(.bottom, 5)
+                }
+            }
+        }
+        //Back Ground Screen AboutView
+//        .frame(maxWidth:.infinity,maxHeight: .infinity)
+//        .background(SwiftUI.Color.backGroudColor.edgesIgnoringSafeArea(.all))
     }
 }
+
 
 struct AboutView_Previews: PreviewProvider {
     static var previews: some View {
